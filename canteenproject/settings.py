3c6231c57a1fa964d4faad398d013e6aaac754dc
ASHWIN SHAJI <ashwinshaji6679@gmail.com>
2022-07-09 09:47:23 +0530

    Update settings.py

--- canteenproject/settings.py
+++ canteenproject/settings.py
@@ -30,8 +30,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['192.168.18.7',
 '127.0.0.1','acms-mits.herokuapp.com']
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 # Application definition
 
 INSTALLED_APPS = [
