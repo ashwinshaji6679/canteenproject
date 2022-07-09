082dccba56d648c9c06774f1345737b66295f924
ASHWIN SHAJI <ashwinshaji6679@gmail.com>
2022-07-09 09:05:31 +0530

    Update settings.py

--- canteenproject/settings.py
+++ canteenproject/settings.py
@@ -12,9 +12,11 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
