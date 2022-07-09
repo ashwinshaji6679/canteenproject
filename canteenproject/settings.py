2f6d3490d476f1ebcac1296591adfb5face83919
ASHWIN SHAJI <ashwinshaji6679@gmail.com>
2022-07-09 09:33:27 +0530

    Update settings.py

--- canteenproject/settings.py
+++ canteenproject/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'canteenproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,"templates")],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,9 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT=os.path.join(BASE_DIR,'canteenapp/staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR,'/canteenapp/staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'canteenapp/static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'/canteenapp/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
