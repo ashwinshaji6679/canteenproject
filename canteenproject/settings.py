fe55c7ee4e9c7cdefc438dfcb223900d8b8a81cc
ASHWIN SHAJI <ashwinshaji6679@gmail.com>
2022-07-21 18:31:54 +0530

    Commit

--- canteenproject/settings.py
+++ canteenproject/settings.py
@@ -127,3 +127,12 @@ USE_TZ = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_MAIL = 'foodkart.acms.mits1@gmail.com'
+SERVER_MAIL = 'foodkart.acms.mits1@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'foodkart.acms.mits1@gmail.com'
+EMAIL_HOST_PASSWORD = 'jernbdcqfpakqlcw'
